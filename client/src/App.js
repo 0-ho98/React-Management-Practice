fdcb28c8689c957ce3ed309f2717d6f273090f7f
(youngho) <(one0374@naver.com)>
2021-01-18 21:02:19 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,9 @@ import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { withStyles } from "@material-ui/core/styles";
+
 
 const styles = (theme) => ({
   root: {
@@ -18,14 +20,24 @@ const styles = (theme) => ({
   table: {
     minWidth: 1080,
   },
+  progress:{
+    margin: theme.spacing(2)
+  }
 });
 
 class App extends React.Component {
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
+  }
+
+  progress = () =>{
+    const {completed} = this.state;
+    this.setState({completed: completed>100 ? 0 : completed +1})
   }
 
   componentDidMount(){
+    this.timer = setInterval(this.progress, 70);
     this.callApi().then(res => this.setState({customers: res})).catch(err => console.log(err));
   }
 
@@ -63,7 +75,13 @@ class App extends React.Component {
                   job={customer.job}
                 />
               );
-            }):""}
+            }):
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+              </TableCell>
+            </TableRow>
+            }
           </TableBody>
         </Table>
       </Paper>
