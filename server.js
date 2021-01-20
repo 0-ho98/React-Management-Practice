cfb4c00da7906fb5aa02804c2157654eeffc6d1a
(youngho) <(one0374@naver.com)>
2021-01-20 16:33:59 +0900

    Create Customer Database Table &  Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,38 +1,32 @@
+const fs = require('fs'); //database.json으로 부터 데이터 베이스 환경설정 정보를 읽어야한다.
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000; //5000포트
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
 
 app.get('/api/customers', (req, res)=>{
-  res.send([
-    {
-      id: 1,
-      image: "https://placeimg.com/64/64/1",
-      name: "박건형",
-      birthday: "980216",
-      gender: "남자",
-      job: "대학생",
-    },
-    {
-      id: 2,
-      image: "https://placeimg.com/64/64/2",
-      name: "강하늘",
-      birthday: "980414",
-      gender: "여자",
-      job: "프로그래머",
-    },
-    {
-      id: 3,
-      image: "https://placeimg.com/64/64/3",
-      name: "지석환",
-      birthday: "980822",
-      gender: "남자",
-      job: "엔지니어",
-    },
-  ]);
+  connection.query(
+  'SELECT * FROM CUSTOMER',
+  (err, rows, fields)=>{
+      res.send(rows); //실제의 데이터는 rows이다.
+  }
+  )
 });
 //app.get의 첫 번째 인자는 path, 두 번째 인자는 callback함수로써
 // 화면에 보여줄 때는 res인자를 사용하여 클라이언트한테 보내준다.
